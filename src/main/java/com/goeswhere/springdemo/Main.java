4f5bf98839e43f31255aa1b11b6f1fbac943ce53
Chris West (Faux) <git@goeswhere.com>
2011-04-21 23:41:04 +0100

    Remove mulletprocessor sillyness

--- src/main/java/com/goeswhere/springdemo/Main.java
+++ src/main/java/com/goeswhere/springdemo/Main.java
@@ -3,23 +3,13 @@ package com.goeswhere.springdemo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.google.common.base.Function;
-
 @Component
 public class Main {
 	@Autowired Printer printer;
 	@Autowired NumberLister lister;
 	@Autowired Fetcher fetcher;
 
-	public static Function<Main, Void> _test;
-
-	public void test() {
-		System.out.println("HI THERE");
-	}
-
 	public void run() {
-		_test.apply(this);
-
 		for (int i : lister)
 			printer.print(fetcher.fetch(i));
 	}
